d3e49c777be910499d21653333b90a954f6c9bb6
ashwini kasat <ashwini.kasat@gmail.com>
2018-02-25 21:49:48 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Ashwini Kasat
+  date: 2/22/2018
+
+*/
+
+$(document).ready(function(){
+    rotate_landingPage_header();
+    
+
+});
+
+function rotate_landingPage_header() {
+    $('.landing_header').hide('slow');
+
+    $('.landing_header').show(2000);
+    
+}
