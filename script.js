63505aefca57f77d49df8f92322ef6fb85d12a77
ashwini kasat <ashwini.kasat@gmail.com>
2018-03-01 09:45:56 -0800

    index page have lots of changes with landing page and new arrengments.
    also css have lots of changes bcoz of that.

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-/*Ashwini Kasat
-  date: 2/22/2018
-
-*/
-
 $(document).ready(function(){
-    rotate_landingPage_header();
-    
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-});
+            // Store hash
+            var hash = this.hash;
 
-function rotate_landingPage_header() {
-    $('.landing_header').hide('slow');
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
 
-    $('.landing_header').show(2000);
-    
-}
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+})
